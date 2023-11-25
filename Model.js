8f10344e72e934d5dd0d08906ab0ebaeb29a008c
akhileshjune64 <akhileshjun64@gmail.com>
2023-11-25 15:28:15 +0530

    allcode

--- Model.js
+++ Model.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    default: "",
-  },
-  email: {
-    type: String,
-    default: "",
-  },
-  message: {
-    type: String,
-    default: "",
-  },
-});
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
 
-const data = mongoose.model("08sep_contact_data", contactSchema);
+      // You can add validation for email format if needed:
+      // match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, "Invalid email format"]
+    },
+    message: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true, // Automatically add createdAt and updatedAt fields
+  }
+);
 
-module.exports = data;
+// Use a singular name for the model
+const ContactForm = mongoose.model("ContactForm", contactSchema);
+
+module.exports = ContactForm;
