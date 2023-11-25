69dd3d0961184d1a5522dd29a672ce2bc7cb4613
akhileshjune64 <akhileshjun64@gmail.com>
2023-11-25 15:06:13 +0530

    allcode

--- Config.js
+++ Config.js
@@ -1,19 +1,14 @@
 const mongoose = require("mongoose");
-
-const username = process.env.myusername;
-const password = process.env.password;
-
-// const cloudDB = `mongodb+srv://${username}:${password}@17hjuly2023.foa55ch.mongodb.net/thebettle`;
-const localDB = "mongodb://0.0.0.0:27017/08sep_contact_data";
-
-mongoose
-  .connect(localDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.Cloud_DB);
     console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    // Optionally, you can throw the error to be caught by the calling function
+    throw error;
+  }
+};
+
+// Call the function to connect to the database
+connectToDatabase();
